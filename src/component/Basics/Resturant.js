ecd6aa2b4a723499a1105e274b504d0e21f278b4
NAZMUS SAKIB <sakibabir52@gmail.com>
2022-03-16 17:08:22 +0600

    food card created

---
+++ src/component/Basics/Resturant.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import './style.css';
+
+const Resturant = () => {
+    return (
+        <div>
+            <div className="card-container">
+                <div className="card">
+                    <div className="card-body">
+                        <span className="card-number card-circle subtle">
+                            1
+                        </span>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Resturant;
