cfc87d243153a84bd155418430676097ce3a18f7
NAZMUS SAKIB <sakibabir52@gmail.com>
2022-03-16 23:38:02 +0600

    restaurant app done

--- src/component/Basics/Resturant.js
+++ src/component/Basics/Resturant.js
@@ -6,15 +6,22 @@ import MenuCard from './MenuCard';
 const Resturant = () => {
     const [menuData, setMenuData] = useState(Menu);
 
+    const handleClick = (category) => {
+        const updatedList = Menu.filter((menu) => {
+            return menu.category === category;
+        });
+        setMenuData(updatedList);
+    };
+
     return (
         <div>
             <nav className="navbar">
                 <div className="btn-group">
-                    <button className="btn-group__item">Breakfast</button>
-                    <button className="btn-group__item">Lunch</button>
-                    <button className="btn-group__item">Evening</button>
-                    <button className="btn-group__item">Dinner</button>
-                    <button className="btn-group__item">All</button>
+                    <button className="btn-group__item" onClick={() => handleClick("breakfast")}>Breakfast</button>
+                    <button className="btn-group__item" onClick={() => handleClick("lunch")}>Lunch</button>
+                    <button className="btn-group__item" onClick={() => handleClick("evening")}>Evening</button>
+                    <button className="btn-group__item" onClick={() => handleClick("dinner")}>Dinner</button>
+                    <button className="btn-group__item" onClick={() => setMenuData(Menu)}>All</button>
                 </div>
             </nav>
             <MenuCard menuData={menuData} key={menuData.id} />
